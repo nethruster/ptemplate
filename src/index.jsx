2948394b5f6b60a70434a42dd3e9fd8762378235
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-08-13 18:03:44 +0200

    :wrench: Finish configuring webpack
    Add styles: sass, autoprefixer
    Add babel env
    Add file-loader

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+require('./scss/index.scss');
+
 class App extends React.Component {
   render () {
     return <p>ptemplate</p>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+  path = require('path'),
+  ExtractTextPlugin = require("extract-text-webpack-plugin"),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
 
-var BUILD_DIR = path.resolve(__dirname, 'src/dist');
-var APP_DIR = path.resolve(__dirname, 'src/');
+  isProduction = process.argv.indexOf('-p') !== -1, // Check if we are in production mode
+  extractStyles = new ExtractTextPlugin('styles.css');
 
-var config = {
+const BUILD_DIR = path.resolve(__dirname, 'src/dist');
+const APP_DIR = path.resolve(__dirname, 'src/');
+
+const config = {
+  devtool: isProduction ? undefined : 'cheap-module-source-map',  
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
@@ -15,16 +21,53 @@ var config = {
     port: 3000,
     contentBase: './src',
     stats: 'minimal',
+    hot: true,
+    inline: true
   },
   module : {
     loaders : [
       {
-        test : /\.jsx?/,
+        test : /\.jsx?$/,
         include : APP_DIR,
         loader : 'babel-loader'
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: isProduction ? extractStyles.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        }) : ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]?[hash]',
+        },
+      },
     ]
-  }
+  },
+  plugins: [
+    isProduction ? extractStyles : new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+      },
+      hash: true,
+      template: './src/index.html',
+    }),
+  ],
 };
 
 module.exports = config;
