266804fa34e99ba97242ee140f7bbb65b89380ce
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-08-14 23:13:18 +0200

    Add content-card component and package scripts
    Refactored CSS and added comments, added namespace to class and id naming.
    
    Add new css vars:
      - content-card background gradient
      - content-card max-width and max-height
      - base border-radius

---
+++ src/components/content-card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ContentCard extends React.Component {
+  render () {
+    return (
+      <div className="pt-content-card">
+        <p>I'm a card</p>
+      </div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import {render} from 'react-dom';
 
 require('./scss/index.scss');
 
+import ContentCard from './components/content-card.jsx';
+
 class App extends React.Component {
   render () {
     return (
-      <p>ptemplate</p>
+      <ContentCard />
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('pt-app'));
