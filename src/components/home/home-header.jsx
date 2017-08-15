420a2b6c774cd5d7add9615ea4e15fffaf61c8e0
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-08-15 14:37:39 +0200

    Improve component structure
    Move home header and body to separate components.
    Add view conteiner (must to because of react) and to isolate the node.
    Add gitkeep to dist folder.

---
+++ src/components/home/home-header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render () {
+    return (
+        <div className="pt-content-card__header flex flex-dc flex-main-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" preserveAspectRatio="none" className="pt-content-card__header__background">
+                <polygon points="0,200 200,0 200,200"/>
+            </svg>
+            <div className="pt-content-card__header__image flex flex-main-center">
+                <img src="https://s-media-cache-ak0.pinimg.com/736x/a9/8d/33/a98d336578c49bd121eeb9dc9e51174d--adobe-illustrator-smileys.jpg" alt="User" />
+            </div>
+            <h1 className="pt-content-card__header__title-name ta-c">Guillem Arias</h1>
+            <p className="pt-content-card__header__title-desc ta-c">Front-end developer</p>
+        </div>
+    );
+  }
+}
---
+++ src/views/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeHeader from '../components/home/home-header.jsx';
+import HomeBody from '../components/home/home-body.jsx';
+
+export default class Home extends React.Component {
+  render () {
+    return (
+        <div className="pt-content-card__home-container flex flex-dc">
+            <HomeHeader />
+            <HomeBody />
+        </div>
+    );
+  }
+}
