6add149741e779a46d4ca546d210f2913f76050e
Claudio4 <me@claudio4.com>
2017-08-16 16:52:15 +0100

    Modify example works

--- src/config.js
+++ src/config.js
@@ -1,14 +1,27 @@
 const works = [
     {
-        title: "Something",
+        title: "Project Title #1",
         img: {
             isSvg: true,
             svgId: "exmaple",
             //Only if not a svg
             url: ""
         },
-        description: "A amazing project",
-        categories: ["Sample", "open source"],
+        description: "Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.",
+        categories: ["Sample", "Open source", "React"],
+        siteUrl: "amazing.project.com",
+        sourceCodeUrl: "github.com/user/project"
+    },
+    {
+        title: "Project Title #2",
+        img: {
+            isSvg: true,
+            svgId: "exmaple",
+            //Only if not a svg
+            url: ""
+        },
+        description: "Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.",
+        categories: ["Sample", "Freeware", "Go"],
         siteUrl: "amazing.project.com",
         sourceCodeUrl: "github.com/user/project"
     }
