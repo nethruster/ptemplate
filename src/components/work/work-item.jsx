8c51d4be9b09ccefa2a7cbdcf4ce37f0e70412e7
Claudio4 <me@claudio4.com>
2017-08-17 22:49:16 +0100

    Modify website and source code buttons in work-item for only appear if required

--- src/components/work/work-item.jsx
+++ src/components/work/work-item.jsx
@@ -3,8 +3,34 @@ import React from 'react';
 import Icon from './../partials/icon.jsx';
 
 export default class WorkItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.getLinksTags = this.getLinksTags.bind(this);
+  }
+  getLinksTags() {
+    var resp = Array(2);
+    if(this.props.work.siteUrl) {
+      resp[0] =  (
+        <a href={this.props.work.siteUrl} key={0} target="_blank" rel="noopener" title="Project Website" className="flex flex-dc flex-full-center">
+          <Icon iconName="web" />
+          <p>Visit Website</p>
+        </a>
+      )
+    } else { resp[0] = null}
+    if(this.props.work.sourceCodeUrl) {
+      resp[1] =  (
+        <a href={this.props.work.sourceCodeUrl} key={1} target="_blank" rel="noopener" title="Project Source Code" className="flex flex-dc flex-full-center">
+          <Icon iconName="code" />
+          <p>Source Code</p>
+        </a>
+      )
+    } else { resp[1] = null}
+    return resp;
+  }
   render() {
     let cateoriyTags = this.props.work.categories.map((category, i) => <span key={i}>{category}</span>);
+    let links = this.getLinksTags();
 
     return (
       <div className="pt-content-card__body__work-items__item flex">
@@ -17,14 +43,7 @@ export default class WorkItem extends React.PureComponent {
           </div>
         </div>
         <div className="pt-content-card__body__work-items__item__buttons flex flex-dc">
-          <a href={this.props.work.siteUrl} target="_blank" rel="noopener" title="Project Website" className="flex flex-dc flex-full-center">
-            <Icon iconName="web" />
-            <p>Visit Website</p>
-          </a>
-          <a href={this.props.work.sourceCodeUrl} target="_blank" rel="noopener" title="Project Source Code" className="flex flex-dc flex-full-center">
-            <Icon iconName="code" />
-            <p>Source Code</p>
-          </a>
+          {links}
         </div>
       </div>
     );
