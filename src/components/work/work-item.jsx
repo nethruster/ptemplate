6295f742b29ee31f5b77842206dc5c515f671a85
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-08-19 23:02:45 +0200

    :hammer: Minor refactor

--- src/components/work/work-item.jsx
+++ src/components/work/work-item.jsx
@@ -6,31 +6,36 @@ export default class WorkItem extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.getLinksTags = this.getLinksTags.bind(this);
+    this.getLinkTags = this.getLinkTags.bind(this);
   }
-  getLinksTags() {
-    var resp = Array(2);
-    if(this.props.work.siteUrl) {
-      resp[0] =  (
-        <a href={this.props.work.siteUrl} key={0} target="_blank" rel="noopener" title="Project Website" className="flex flex-dc flex-full-center">
+
+  getLinkTags() {
+    let linkTags = [];
+
+    if (this.props.work.siteUrl) {
+      linkTags[0] = (
+        <a href={this.props.work.siteUrl} key={resp.length} target="_blank" rel="noopener" title="Project Website" className="flex flex-dc flex-full-center">
           <Icon iconName="web" />
           <p>Visit Website</p>
         </a>
       )
-    } else { resp[0] = null}
-    if(this.props.work.sourceCodeUrl) {
-      resp[1] =  (
-        <a href={this.props.work.sourceCodeUrl} key={1} target="_blank" rel="noopener" title="Project Source Code" className="flex flex-dc flex-full-center">
+    } else { linkTags[0] = null }
+
+    if (this.props.work.sourceCodeUrl) {
+      linkTags[1] = (
+        <a href={this.props.work.sourceCodeUrl} key={resp.length} target="_blank" rel="noopener" title="Project Source Code" className="flex flex-dc flex-full-center">
           <Icon iconName="code" />
           <p>Source Code</p>
         </a>
       )
-    } else { resp[1] = null}
-    return resp;
+    } else { linkTags[1] = null }
+
+    return linkTags;
   }
+
   render() {
-    let cateoriyTags = this.props.work.categories.map((category, i) => <span key={i}>{category}</span>);
-    let links = this.getLinksTags();
+    let cateoryTags = this.props.work.categories.map((category, i) => <span key={i}>{category}</span>);
+    let linkTags = this.getLinkTags();
 
     return (
       <div className="pt-content-card__body__work-items__item flex">
@@ -39,11 +44,11 @@ export default class WorkItem extends React.PureComponent {
           <h4>{this.props.work.title}</h4>
           <p>{this.props.work.description}</p>
           <div className="pt-content-card__body__work-items__item__data__tags">
-            {cateoriyTags}
+            {cateoryTags}
           </div>
         </div>
         <div className="pt-content-card__body__work-items__item__buttons flex flex-dc">
-          {links}
+          {linkTags}
         </div>
       </div>
     );
