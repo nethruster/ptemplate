618be06d6ef912c93eceabe43d0acdf97f5dad2a
Claudio4 <me@claudio4.com>
2017-09-05 15:21:55 +0100

    Modify how the data is collected in the contact form

--- src/components/contact/contact-body.jsx
+++ src/components/contact/contact-body.jsx
@@ -11,20 +11,21 @@ export default class ContactBody extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.formInputs = {
-      name: null,
-      email: null,
-      message: null,
-      captcha: null 
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
     };
+    this.captcha = null;
+
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onCaptchaChange = this.onCaptchaChange.bind(this);
-    this.setInputReference = this.setInputReference.bind(this);
+    this.onInputValueChange = this.onInputValueChange.bind(this);
   }
   
   onFormSubmit(e) {
     e.preventDefault();
-    this.formInputs.captcha.execute();
+    this.captcha.execute();
   }
 
   onCaptchaChange(value) {
@@ -33,14 +34,16 @@ export default class ContactBody extends React.PureComponent {
     }
 
     SendToForm(
-      this.formInputs.name.value,
-      this.formInputs.email.value,
-      this.formInputs.message.value,
+      this.state.name,
+      this.state.email,
+      this.state.message,
       value
     )
   }
-  setInputReference(el) {
-    this.formInputs[el.id] = el
+  onInputValueChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
   } 
 
   render() {
@@ -58,7 +61,7 @@ export default class ContactBody extends React.PureComponent {
         <div className="pt-content-card__body__contact__form flex flex-main-center">
           <form className="flex flex-dc flex-full-center" onSubmit={this.onFormSubmit}>
             <div className="pt-content-card__body__contact__form__row flex flex-dc flex-main-center">
-              <input id="name" className="pt-content-card__body__contact__form__input" type="text" ref={this.setInputReference}/>
+              <input id="name" className="pt-content-card__body__contact__form__input" type="text" onChange={this.onInputValueChange}/>
               <label htmlFor="name" className="pt-content-card__body__contact__form__label">What's your name?</label>
               <svg className="line" viewBox="0 0 40 2" preserveAspectRatio="none">
                 <path d="M0 1 L40 1" />
@@ -68,7 +71,7 @@ export default class ContactBody extends React.PureComponent {
               </svg>
             </div>
             <div className="pt-content-card__body__contact__form__row flex flex-dc flex-main-center">
-              <input id="email" className="pt-content-card__body__contact__form__input" type="email" ref={this.setInputReference}/>
+              <input id="email" className="pt-content-card__body__contact__form__input" type="email" onChange={this.onInputValueChange}/>
               <label htmlFor="email" className="pt-content-card__body__contact__form__label">What's your email?</label>
               <svg className="line" viewBox="0 0 40 2" preserveAspectRatio="none">
                 <path d="M0 1 L40 1" />
@@ -78,7 +81,7 @@ export default class ContactBody extends React.PureComponent {
               </svg>
             </div>
             <div className="pt-content-card__body__contact__form__row flex flex-dc flex-main-center">
-              <textarea id="message" className="pt-content-card__body__contact__form__textarea" rows="6" ref={this.setInputReference}/>
+              <textarea id="message" className="pt-content-card__body__contact__form__textarea" rows="6" onChange={this.onInputValueChange}/>
               <label htmlFor="message" className="pt-content-card__body__contact__form__label">Please, explain yourself:</label>
               <svg className="line" viewBox="0 0 40 2" preserveAspectRatio="none">
                 <path d="M0 1 L40 1" />
@@ -90,7 +93,7 @@ export default class ContactBody extends React.PureComponent {
 
             <div className="pt-content-card__body__contact__form__row flex flex-dc flex-main-center">
               <ReCAPTCHA
-                ref={(el) => {this.formInputs.captcha = el}}
+                ref={(el) => {this.captcha = el}}
                 className="recaptcha"
                 size="invisible"
                 sitekey="6LcBOC8UAAAAAM9YRyBp1RR-1NnwvMU8UDsR63Vu"
