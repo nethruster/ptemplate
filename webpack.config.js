3a9ad6f76e8ab37ac7960db1f19d6d0791fa95e7
Claudio4 <me@claudio4.com>
2017-09-29 19:47:08 +0100

    Add build-gz script to package.json to geneare a .gz for each build file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
   ExtractTextPlugin = require("extract-text-webpack-plugin"),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   CopyWebpackPlugin = require('copy-webpack-plugin'),
+  CompressionPlugin = require("compression-webpack-plugin")
 
   isProduction = process.argv.indexOf('-p') !== -1, // Check if we are in production mode
   extractStyles = new ExtractTextPlugin('styles.css');
@@ -10,70 +11,82 @@ const webpack = require('webpack');
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
 
-const config = {
-  devtool: isProduction ? undefined : 'cheap-module-source-map',  
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'ptemplate.js'
-  },
-  devServer: {
-    host: 'localhost',
-    historyApiFallback: true,
-    port: 3000,
-    contentBase: './src',
-    stats: 'minimal',
-    hot: true,
-    inline: true
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?$/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: isProduction ? extractStyles.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'postcss-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        }) : ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|ico|xml)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'assets/[name].[ext]?[hash]',
+module.exports = env => {
+  const config = {
+    devtool: isProduction ? undefined : 'cheap-module-source-map',  
+    entry: APP_DIR + '/index.jsx',
+    output: {
+      path: BUILD_DIR,
+      filename: 'ptemplate.js'
+    },
+    devServer: {
+      host: 'localhost',
+      historyApiFallback: true,
+      port: 3000,
+      contentBase: './src',
+      stats: 'minimal',
+      hot: true,
+      inline: true
+    },
+    module : {
+      loaders : [
+        {
+          test : /\.jsx?$/,
+          include : APP_DIR,
+          loader : 'babel-loader'
         },
-      },
-    ]
-  },
-  plugins: [
-    isProduction ? extractStyles : new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      minify: {
-        collapseWhitespace: true,
-      },
-      hash: true,
-      template: './src/index.html',
-    }),
-  ],
-};
-if(isProduction) {
-  config.plugins[config.plugins.length] = new CopyWebpackPlugin([
-    { from: path.join(__dirname, 'src', 'assets', 'icons'), to: path.join(__dirname, 'dist', 'assets', 'icons') }
-  ]);
+        {
+          test: /\.scss$/,
+          use: isProduction ? extractStyles.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+              },
+              {
+                loader: 'postcss-loader',
+              },
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          }) : ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|ico|xml)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[ext]?[hash]',
+          },
+        },
+      ]
+    },
+    plugins: [
+      isProduction ? extractStyles : new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        minify: {
+          collapseWhitespace: true,
+        },
+        hash: true,
+        template: './src/index.html',
+      }),
+    ],
+  };
+
+  if(isProduction) {
+    config.plugins[config.plugins.length] = new CopyWebpackPlugin([
+      { from: path.join(__dirname, 'src', 'assets', 'icons'), to: path.join(__dirname, 'dist', 'assets', 'icons') }
+    ]);
+  }
+
+  if(env.compress) {
+    config.plugins[config.plugins.length] = new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.(html|js|css|png|jpg|json|svg|xml)$/,
+      minRatio: 1.0
+    });
+  }
+
+  return config;
 }
-module.exports = config;
