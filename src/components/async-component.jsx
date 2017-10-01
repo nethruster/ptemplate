6226e8c972fb535a6f1cb2f1fb4a1c1ec22c1ffd
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-10-01 21:04:54 +0200

    Implement view-loader

--- src/components/async-component.jsx
+++ src/components/async-component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import ViewLoading from './views/shared/view-loading/view-loading.js';
+import ViewLoading from './partials/view-loading.jsx';
 
 // Huge thanks to Hassan Ali on https://hackernoon.com/code-splitting-for-react-router-with-webpack-and-hmr-bb509968e86f
 
@@ -21,7 +21,7 @@ export default (loader, collection) => (
         }
 
         render() {
-            return this.state.component ? <this.state.component { ...this.props } { ...collection } /> : null;
+            return this.state.component ? <this.state.component { ...this.props } { ...collection } /> : <ViewLoading />;
         }
     }
 );
