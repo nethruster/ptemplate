efd4d03940c30b6821bc53b1b14de2b4b2a2d369
Travis CI <support@nethruster.com>
2017-11-02 00:44:54 +0000

    Deploy to GitHub Pages: 27673dde1d37119cc243ca34daea338b4b553004

---
+++ README.md
@@ -0,0 +1,46 @@
+# ptemplate
+[![Dependencies](https://david-dm.org/nethruster/ptemplate.svg)](https://david-dm.org/nethruster/ptemplate) [![DevDependencies](https://david-dm.org/nethruster/ptemplate/dev-status.svg)](https://david-dm.org/nethruster/ptemplate?type=dev)
+
+A personal website template that's not a pain in the ass to setup and use.
+
+### Requirements
+- You need to have `yarn` (**recommended**) or `npm` installed.
+
+### Useful commands
+
+#### Yarn
+- `yarn` - Install and update dependencies.
+- `yarn dev` - Launch local web server with hot compiling. Ideal to work on the project easily.
+- `yarn build` - Build the website in production mode to the `dist/` folder.
+- `yarn build-gz` - Builds a gzip compressed version of the built app in the dist/ folder.
+
+#### NPM
+- `npm install` - Install and update dependencies.
+- `npm run dev` - Launch local web server with hot compiling. Ideal to work on the project easily.
+- `npm run build` - Build the website in production mode to the `dist/` folder.
+- `npm run build-gz` - Builds a gzip compressed version of the built app in the dist/ folder.
+
+## Customization
+
+Note: To apply all of these changes you must rebuild the app.
+
+### Favicons
+You can replace them at `src/assets/icons/`.
+
+### Avatar
+You can replace it at `src/assets/`. If the name or the extension is different to "avatar.svg", you need to modify it in `src/config.js` and `src/assets-imports.js`.
+
+### Title
+Change the <title> tag in `src/index.html`.
+
+### Most of the UI (description, work content, contact icons...)
+Modify `src/config.js`. You can add, remove or modify its contents.
+
+### ReCATPCHA public key
+Change it in `src/config.js`.
+
+### Translations
+Customize them in the `src/assets/lang/` folder. Add a new lang file named by the [IEC_15897](https://en.wikipedia.org/wiki/ISO/IEC_15897) standard, paste contents from your source locale and translate it. To activate a language copy the contents from the source locale to `src/asssets/lang.json`.
+
+### Add icons
+Add new icon paths with the desired ID in the `src/assets/icons.svg` file.
---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const webpack = require('webpack');
+  path = require('path'),
+  ExtractTextPlugin = require("extract-text-webpack-plugin"),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  CompressionPlugin = require("compression-webpack-plugin")
+
+  isProduction = process.argv.indexOf('-p') !== -1, // Check if we are in production mode
+  extractStyles = new ExtractTextPlugin('styles.css');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+module.exports = env => {
+  const config = {
+    devtool: isProduction ? undefined : 'cheap-module-source-map',  
+    entry: {
+      'main': APP_DIR + '/index.jsx',
+      'config': APP_DIR + '/config',
+      'lang': APP_DIR + '/assets/lang/lang',
+      'vendor': [
+        'preact',
+        'preact-compat',
+        'react-ink',
+        'react-router-dom'
+      ]
+    },
+    output: {
+      path: BUILD_DIR,
+      publicPath: '/',
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    },
+    devServer: {
+      host: 'localhost',
+      historyApiFallback: true,
+      port: 3000,
+      contentBase: './src',
+      stats: 'minimal',
+      hot: true,
+      inline: true
+    },
+    module : {
+      loaders : [
+        {
+          test : /\.jsx?$/,
+          include : APP_DIR,
+          loader : 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: isProduction ? extractStyles.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+              },
+              {
+                loader: 'postcss-loader',
+              },
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          }) : ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|ico|xml)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[ext]?[hash]',
+          },
+        },
+      ]
+    },
+    "resolve": {
+      "alias": {
+        "react": "preact-compat",
+        "react-dom": "preact-compat"
+      },
+      "modules": [
+        APP_DIR + '/assets/lang/',
+        'node_modules'
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development')
+        }
+      }),
+      isProduction ? extractStyles : new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        minify: {
+          collapseWhitespace: true,
+        },
+        hash: true,
+        template: './src/index.html',
+      }),
+      new webpack.optimize.CommonsChunkPlugin({names: ['vendor', 'config', 'lang']})
+    ],
+  };
+
+  if(isProduction) {
+    config.plugins[config.plugins.length] = new CopyWebpackPlugin([
+      { from: path.join(__dirname, 'src', 'assets', 'icons'), to: path.join(__dirname, 'dist', 'assets', 'icons') }
+    ]);
+  }
+
+  if(env && env.compress) {
+    config.plugins[config.plugins.length] = new CompressionPlugin({
+      algorithm: "gzip",
+      test: /\.(html|js|css|png|jpg|json|svg|xml)$/
+    });
+  }
+
+  return config;
+}
