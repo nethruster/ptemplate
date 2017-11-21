b5e3a1f13d29bbd43f331564549396136d24cce9
gariasf <gariasfa7@alumnes.ub.edu>
2017-11-21 19:53:26 +0100

    Remove css custom properties
    Add simple browser feature check

--- src/index.jsx
+++ src/index.jsx
@@ -20,4 +20,9 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('pt-app'))
+if (typeof Promise !== 'undefined' && Promise.toString().indexOf('[native code]') !== -1) {
+  render(<App />, document.getElementById('pt-app'))
+} else {
+  window.alert('Please, use an updated browser like Google Chrome or Firefox if you want to use this website properly.')
+  console.error('This browser doesn\'t support necessary web technology for this site to work, please, use an updated browser like Google Chrome or Firefox if you want to use this website properly.')
+}
