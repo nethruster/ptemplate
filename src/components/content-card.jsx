943f0a91df1ae86c88714b0c06792337ee73d8f4
Claudio Yanes <me@claudio4.com>
2020-06-25 22:29:44 +0100

    Add prefecth for every page

--- src/components/content-card.jsx
+++ src/components/content-card.jsx
@@ -8,11 +8,11 @@ export default class ContentCard extends React.Component {
     return (
       <div className='pt-content-card'>
         <Switch>
-          <Route exact path='/' component={asyncComponent(() => import(/*webpackChunkName: "home"*/'../views/home.jsx').then(module => module.default))} />
-          <Route exact path='/about' component={asyncComponent(() => import(/*webpackChunkName: "about"*/'../views/about.jsx').then(module => module.default))} />
-          <Route exact path='/work' component={asyncComponent(() => import(/*webpackChunkName: "work"*/'../views/work.jsx').then(module => module.default))} />
-          <Route exact path='/contact' component={asyncComponent(() => import(/*webpackChunkName: "contact"*/'../views/contact.jsx').then(module => module.default))} />
-          <Route component={asyncComponent(() => import(/*webpackChunkName: "404"*/'../views/404.jsx').then(module => module.default))} />
+          <Route exact path='/' component={asyncComponent(() => import(/* webpackPrefetch: true, webpackChunkName: "home" */'../views/home.jsx').then(module => module.default))} />
+          <Route exact path='/about' component={asyncComponent(() => import(/* webpackPrefetch: true, webpackChunkName: "about" */'../views/about.jsx').then(module => module.default))} />
+          <Route exact path='/work' component={asyncComponent(() => import(/* webpackPrefetch: true, webpackChunkName: "work" */'../views/work.jsx').then(module => module.default))} />
+          <Route exact path='/contact' component={asyncComponent(() => import(/* webpackPrefetch: true, webpackChunkName: "contact" */'../views/contact.jsx').then(module => module.default))} />
+          <Route component={asyncComponent(() => import(/* webpackChunkName: "404" */'../views/404.jsx').then(module => module.default))} />
         </Switch>
       </div>
     )
