33716acf5e946a12e2a62d6336828f21742addb7
Claudio Yanes <me@claudio4.com>
2020-06-25 23:42:26 +0100

    Add defer to chuncks in pre-render
    When pren-rendered with react-snap the page chunks (home.js, 404.js...)
    are loaded with script tags in the head, blocking the rendering , now
    a defer property is added to those script tags to prevent them from blocking
     the page render

---
+++ scripts/add-defer-to-chunks.js
@@ -0,0 +1,37 @@
+const path = require('path'), fs = require('fs')
+
+function fromDir(startPath, filter, callback) {
+  if (!fs.existsSync(startPath)) {
+    console.error(`Directory "${startPath}" doesn't exists`)
+    return
+  }
+
+  var files = fs.readdirSync(startPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(startPath, files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      fromDir(filename, filter, callback);
+    } else if (filter.test(filename))
+      callback(filename);
+  };
+};
+
+fromDir('dist', /\.html$/, (filename) => {
+  console.log('Adding defer to chunks in ' + filename)
+  fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file "${filename}": `, err)
+  }
+  fs.writeFile(
+      filename,
+      data.replace(
+          /<script charset="utf-8" src=/g,
+          '<script charset="utf-8" defer src='),
+      err => {
+        if (err) {
+          console.error(`Error writing file "${filename}": `, err)
+        }
+      })
+  })
+})
